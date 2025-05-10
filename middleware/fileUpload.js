75e011f112df1a75c52b0e85b2b14324a659631d
Bright Bediako <bright.bediako.dev@gmail.com>
2025-05-10 09:20:07 +0000

    modified files

--- middleware/fileUpload.js
+++ middleware/fileUpload.js
@@ -1,25 +1,29 @@
 import multer from "multer";
 import path from "path";
+import fs from "fs";
+
+fs.mkdirSync("uploads", { recursive: true });
 
 // multer file upload configuration
 const storage = multer.diskStorage({
-  destination: (req, fle, cb) => {
+  destination: (req, file, cb) => {
     cb(null, "uploads/");
   },
   filename: (req, file, cb) => {
     const generateUniqueName =
-      Date.now() + "_" + Math.round(Math.random() * 1E9);
+      Date.now() + "_" + Math.round(Math.random() * 1e9);
     const ext = path.extname(file.originalname);
     const fileOriginalName = req.body.name || "file";
     cb(null, fileOriginalName + "_" + generateUniqueName + ext);
   },
 });
 
-
 // check for file type
 const checkFileType = (req, file, cb) => {
   const allowedTypes = /jpeg|jpg|png/;
-  const extName = allowedTypes.test(path.extname(file.originalname).toLowerCase());
+  const extName = allowedTypes.test(
+    path.extname(file.originalname).toLowerCase()
+  );
   const mimeType = allowedTypes.test(file.mimetype);
 
   if (extName && mimeType) {
@@ -29,11 +33,10 @@ const checkFileType = (req, file, cb) => {
   }
 };
 
-
 //  check for file size
 const upload = multer({
   storage,
-  limits: { filesize: 1024 * 1024 * 10 },
+  limits: { fileSize: 1024 * 1024 * 10 },
   fileFilter: checkFileType,
 });
 
